9ca3df6fbe0b0544fa0af4dbfda709cbfd8cef2e
P Harika <90428908+harika-pendli@users.noreply.github.com>
2022-02-26 14:24:44 -0500

    Add files via upload

---
+++ sol_4b.py
@@ -0,0 +1,145 @@
+import layers
+import numpy as np
+import matplotlib.pyplot as plt
+import data_generators
+
+class Network(layers.BaseNetwork):
+    #TODO: you might need to pass additional arguments to init for prob 2, 3, 4 and mnist
+    def __init__(self, data_layer,parameters=None):
+        # you should always call __init__ first 
+        super().__init__()
+        #TODO: define your network architecture here
+        self.hidden_units= parameters["hidden_units"]
+        self.hidden_layers=parameters["hidden_layers"]
+        
+        # For prob 3 and 4:
+        # layers.ModuleList can be used to add arbitrary number of layers to the network
+        # e.g.:
+        self.MY_MODULE_LIST = layers.ModuleList()
+        self.MY_MODULE_LIST.append(data_layer)
+        
+        for i in range(self.hidden_layers+1):
+             self.MY_MODULE_LIST.append(layers.Linear(self.MY_MODULE_LIST[-1],self.hidden_units))
+             self.MY_MODULE_LIST.append(layers.Bias(self.MY_MODULE_LIST[-1]))
+             self.MY_MODULE_LIST.append(layers.Relu(self.MY_MODULE_LIST[-1]))
+        
+        self.MY_MODULE_LIST.append(layers.Linear(self.MY_MODULE_LIST[-1],1))
+        self.MY_MODULE_LIST.append(layers.Bias(self.MY_MODULE_LIST[-1]))
+        self.MY_MODULE_LIST.append(layers.Sigmoid(self.MY_MODULE_LIST[-1]))
+
+        #TODO: always call self.set_output_layer with the output layer of this network (usually the last layer)
+        self.set_output_layer(self.MY_MODULE_LIST[-1])
+
+class Trainer:
+    def __init__(self):
+        pass
+    
+    def define_network(self, data_layer, parameters=None):
+        '''
+        For prob 2, 3, 4 and mnist:
+        parameters is a dict that might contain keys: "hidden_units" and "hidden_layers".
+        "hidden_units" specify the number of hidden units for each layer. "hidden_layers" specify number of hidden layers. 
+        Note: we might be testing if your network code is generic enough through define_network. Your network code can be even more general, but this is the bare minimum you need to support.
+        Note: You are not required to use define_network in setup function below, although you are welcome to.
+        '''
+        hidden_units = parameters["hidden_units"] #needed for prob 2, 3, 4, mnist
+        hidden_layers = parameters["hidden_layers"] #needed for prob 3, 4, mnist
+        #TODO: construct your network here
+        network = Network(data_layer, parameters=parameters )
+        return network
+    
+    def setup(self, training_data):
+        x, y = training_data
+        self.training_data=training_data
+        #TODO: define input data layer
+        self.data_layer = layers.Data(x)
+        #TODO: construct the network. you don't have to use define_network.
+        hidden_layers = 3
+        hidden_units = 15
+        parameters={"hidden_units": hidden_units,"hidden_layers":hidden_layers}  ######################change as needed
+        self.network = Network(self.data_layer,parameters=parameters)
+        #TODO: use the appropriate loss function here
+        self.loss_layer = layers.CrossEntropy(self.network.get_output_layer(),y) 
+        #TODO: construct the optimizer class here. You can retrieve all modules with parameters (thus need to be optimized be the optimizer) by "network.get_modules_with_parameters()"
+        self.optim = layers.SGDSolver(0.005, self.network.get_modules_with_parameters())   ###############change as needed
+        return self.data_layer, self.network, self.loss_layer, self.optim
+    
+    def train_step(self):
+        # TODO: train the network for a single iteration
+        # you have to return loss for the function
+        loss= self.loss_layer.forward()
+        self.loss_layer.backward()
+        self.optim.step()
+        return loss
+    def get_num_iters_on_public_test(self):
+        #TODO: adjust this number to how much iterations you want to train on the public test dataset for this problem.
+        return 10000
+    
+    def train(self, num_iter):
+        train_losses = []
+        #TODO: train the network for num_iter iterations. You should append the loss of each iteration to train_losses.
+        for i in range(num_iter):
+            train_losses.append(self.train_step())
+
+        # you have to return train_losses for the function
+        return train_losses
+    
+#DO NOT CHANGE THE NAME OF THIS FUNCTION
+def main(test=False):
+
+    #setup the trainer
+    trainer = Trainer()
+    
+    #DO NOT REMOVE THESE IF/ELSE
+    if not test:
+        # For prob mnist: example data loading code:
+        # mnist = np.load("mnist.pkl", allow_pickle=True)
+        # x_train = mnist["training_images"]
+        # y_train = mnist["training_labels"]
+        # x_test = mnist["test_images"]
+        # y_test = mnist["test_labels"]
+
+
+        dataset= data_generators.data_4b()
+        x_train = dataset["train"][0]
+        x_test= dataset["test"][0]
+        y_train = dataset["train"][1]
+        y_test= dataset["test"][1]
+
+        # Your code goes here.
+
+        trainer.setup(dataset["train"])
+        
+        print("Module list:",trainer.network.get_modules_with_parameters())
+        
+
+
+        iter=10000
+        loss=trainer.train(iter)
+        print(loss[-1])
+
+        iterations = [i for i in range(1,iter+1)]
+        plt.title("Loss covergence with iterations")
+        plt.xlabel("No. of Iterations")
+        plt.ylabel(" Loss ")
+        plt.plot(iterations,loss)
+        plt.show()
+
+        trainer.data_layer.set_data(dataset["test"][0])
+        pred = trainer.network.forward()
+
+        
+        
+        # For prob mnist: you can use this snippet to save the network weight:
+        # np.savez("mnist_weight.npz", weight=trainer.network.state_dict())
+    else:
+        #DO NOT CHANGE THIS BRANCH! This branch is used for autograder.
+        out = {
+            'trainer': trainer
+        }
+        return out
+
+if __name__ == "__main__":
+    main()
+    pass
+ 
